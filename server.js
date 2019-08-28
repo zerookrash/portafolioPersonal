7a9745109b030c78ec0e8323cf5030eadeb83adb
zerookrash <zerookrash@me.com>
2019-08-27 21:35:24 -0500

    Quitando /dist en el servidor.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + 'index.html'));
 });
 
 app.post('/formulario', (req, res) => {
